6e32463c044bc5e8ebdffdddd57235f45a3566a6
Maximilian Roll <maxroll54@gmail.com>
2020-01-08 15:14:24 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title> Freezer Management | Login </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300|Source+Sans+Pro:200,300,400&display=swap" rel="stylesheet">
+    
+    <!--JavaScript-->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/utils/Draggable.min.js"></script>
+    <script src="js/parallax.js"></script>
+</head>
+
+
+<body>
+   <?php
+    
+    $nameErr = $passErr = "";
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(empty($_POST['username'])){
+            $nameErr = "Enter a Username";
+            if(empty($_POST['pass'])){
+                $passErr = "Enter a Password";
+            }
+        }
+        elseif(empty($_POST['pass'])){
+            $passErr = "Enter a password";
+            if(empty($_POST['username'])){
+                $nameErr = "Enter a Username";
+            }
+        }
+        else{
+            include "login.php";
+        }
+    }
+    ?>
+    <div id="container">
+        <div class="wrapper">
+            <span class="title">
+                FREEZER MANAGEMENT
+            </span>
+            <form method="post" class=login action="<?php echo $_SERVER["PHP_SELF"];?>">
+                <div class="usr input">
+                    <span>Username<span class="error"><?php echo $nameErr ?></span></span>
+                    <input type="text" name="username" class="form" value="<?php if (isset($_POST['username'])) echo $_POST['username'];?>">
+                </div>
+                <div class="pwd input">
+                    <span>Password<span class="error"><?php echo $passErr ?></span></span>
+                    <input type="password" name="pass" class="form">
+                </div>
+                <input type="submit" name="login" value="LOGIN" class="btn">
+            </form>
+        </div>
+    </div>
+</body>
---
+++ js/parallax.js
@@ -0,0 +1,49 @@
+function delay(URL) {
+    $("#container").removeClass("fade");
+    $("#container").addClass("fade");
+    setTimeout(function () {
+        window.location = URL;
+    }, 400)
+};
+
+$(document).ready(function () {
+    // Retrieve container position relative to viewport
+var rect = $('#container')[0].getBoundingClientRect();
+// Create mouse object to keep track of mouse coordinates
+var mouse = {x: 0, y: 0, moved: false};
+
+$("#container").mousemove(function(e) {
+  // set moved property to true
+  mouse.moved = true;
+  // calculate mouse coordinates
+  mouse.x = e.clientX - rect.left;
+  mouse.y = e.clientY - rect.top;
+});
+ 
+// Ticker event will be called on every frame
+TweenLite.ticker.addEventListener('tick', function(){
+  // if mouse is moved call parallax function
+  if (mouse.moved){    
+    parallaxIt(".wrapper", -30);
+    parallaxIt(".login", -10);
+    parallaxIt(".content-wrapper", -50)
+    parallaxIt(".menu", -5)
+    parallaxIt(".menulist", -10)
+  }
+  // set moved property to false on each frame so parallax function won't be called more than once
+  mouse.moved = false;
+});
+
+// Simplest part, take mouse coordinates and container dimensions and animate elements
+function parallaxIt(target, movement) {
+  TweenMax.to(target, 0.3, {
+    x: (mouse.x - rect.width / 2) / rect.width * movement,
+    y: (mouse.y - rect.height / 2) / rect.height * movement
+  });
+}
+
+// Recaclulate container dimensions on resize and scroll
+$(window).on('resize scroll', function(){
+  rect = $('#container')[0].getBoundingClientRect();
+})
+});
