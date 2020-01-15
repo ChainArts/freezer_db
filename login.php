6f7c36d97e08bfbbdcf347b1718c2e0c57c8c35b
Maximilian Roll <maxroll54@gmail.com>
2020-01-15 15:29:10 +0100

    added login and config file

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$username = mysqli_real_escape_string($link, $_POST['username']);
+$result = mysqli_query($link, "SELECT * FROM userdata WHERE username='$username'");
+if($result->num_rows == 0){
+    $nameErr = "Incorrect Username or Password";
+}
+else{
+    $user = mysqli_fetch_assoc($result);
+    if(password_verify($_POST['pass'],$user['password'])){
+        
+        $_SESSION["USR_Name"] = $user['username'];
+        $_SESSION["USR_PASS"] = $user['password'];
+        $_SESSION["USR_VNAME"] = $user['firstname'];
+        $_SESSION["USR_SNAME"] = $user['lastname'];
+    }
+    else{
+        $nameErr = "Incorrect Username or password";
+    }
+}
+}
+mysqli_free_result($result);
+?>
