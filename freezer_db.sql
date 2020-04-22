e6167b581905875bb4735c99b44162154e113d47
Maximilian Roll <maxroll54@gmail.com>
2020-04-22 19:32:04 +0200

    Updated Database

--- freezer_db.sql
+++ freezer_db.sql
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `freezer_db`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `freezer_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `freezer_db`;
-
---
 -- Table structure for table `contents`
 --
 
@@ -31,14 +23,15 @@ DROP TABLE IF EXISTS `contents`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contents` (
-  `item_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
-  `description` varchar(45) DEFAULT NULL,
-  `date` varchar(45) DEFAULT NULL,
+  `notes` varchar(1000) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
   `user` varchar(45) DEFAULT NULL,
   `type` varchar(45) DEFAULT NULL,
+  `location` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,6 +40,7 @@ CREATE TABLE `contents` (
 
 LOCK TABLES `contents` WRITE;
 /*!40000 ALTER TABLE `contents` DISABLE KEYS */;
+INSERT INTO `contents` VALUES (1,'Goldbrasse','1 STK','2020-04-22 14:49:54','admin','Fisch','1'),(2,'Schweineschnitzel','2 STK Pute','2020-04-22 15:13:41','admin','Fleisch','6'),(4,'fadsf','afsdsaf','2020-04-22 16:25:32','admin','dfassdf','14');
 /*!40000 ALTER TABLE `contents` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,13 +52,14 @@ DROP TABLE IF EXISTS `freezer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `freezer` (
-  `frz_id` int(11) NOT NULL,
+  `frz_id` int(11) NOT NULL AUTO_INCREMENT,
   `location` varchar(45) DEFAULT NULL,
   `model` varchar(45) DEFAULT NULL,
   `capacity` int(11) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
+  `brand` varchar(45) DEFAULT NULL,
+  `status` int(11) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`frz_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +68,7 @@ CREATE TABLE `freezer` (
 
 LOCK TABLES `freezer` WRITE;
 /*!40000 ALTER TABLE `freezer` DISABLE KEYS */;
+INSERT INTO `freezer` VALUES (1,'Kitchen','AGS 0993 F',400,'AEG',1),(2,'Kitchen','TF 140.1',600,'Novamatic',0),(3,'Kitchen','TG095',300,'Electrolux',0),(4,'Kitchen','GS54NAW42',800,'Siemens',0),(5,'Kitchen','GKN ECO 18',800,'Bauknecht',0),(6,'Kitchen','GS58NAWDPH ',800,'Siemens',1),(7,'Kitchen','LTGN 268',800,'Liebherr',0),(14,'Kitchen 1','Ag429F',5,'Siemens',1),(15,'Cellar','ag41092',300,'Siemens',0);
 /*!40000 ALTER TABLE `freezer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,13 +80,14 @@ DROP TABLE IF EXISTS `userdata`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userdata` (
-  `usr_id` int(11) NOT NULL,
+  `usr_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) DEFAULT NULL,
   `firstname` varchar(45) DEFAULT NULL,
   `lastname` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `usr_lvl` int(2) DEFAULT 0,
   PRIMARY KEY (`usr_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,6 +96,7 @@ CREATE TABLE `userdata` (
 
 LOCK TABLES `userdata` WRITE;
 /*!40000 ALTER TABLE `userdata` DISABLE KEYS */;
+INSERT INTO `userdata` VALUES (0,'admin','Maximilian','Roll','$2y$10$udCcydVvo0Fw.dJZQpXZCuLVGGFZAr78gJjNOmd8VXtek7kwpSNzC',1),(1,'hansi','Hansi','Huber','$2y$10$7fRb8h3WIy/cdyqDcL1YzeszIofq00kfOYG1O8CnwX/ApLUeFWs5m',0),(2,'NeoPlays','Martin','Platajs','$2y$10$wkgILISFUBr56D.EEX78I.bKNNi4kp/wv78Fzpw09BQGQFxjTeOIq',0),(5,'maroan','Marion','Roll','$2y$10$mglDO2cKeb95DTGbuZ817O3xa.1Aoo63pfIbKFOBXf3kXZW7Sykfm',0),(6,'hans','hansi','huber','$2y$10$KO9fsylsv0k4V5.uPHpBXO70lA/3zzyaMi1wQvHs919SyTFisDmX6',0);
 /*!40000 ALTER TABLE `userdata` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -111,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-11 15:53:31
+-- Dump completed on 2020-04-22 19:31:05
